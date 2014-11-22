ec708a0585b56354f41a27ce9019c88415aefba7
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2014-11-22 18:20:55 +0100

    Create roles.js

---
+++ scripts/roles.js
@@ -0,0 +1,14 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('roles'); // -> 'a thing'
+ */
+
+var roles = {
+    // TODO - INSERT ROLES HERE
+    // 'FOO': require('role_FOO'),
+}
+
+module.exports = roles;
