930e433ccf79c545491d30d6e5fb1b0e341effc0
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2014-12-08 23:10:16 +0100

    Learn spawner how to process objects + spawn fixes

--- scripts/stage_spawners.js
+++ scripts/stage_spawners.js
@@ -5,6 +5,7 @@
  * You can import it from another modules like this:
  * var mod = require('spawners'); // -> 'a thing'
  */
+var _ = require('lodash');
 var generics = require('_generics');
 var roles = require('roles');
 
@@ -21,19 +22,35 @@ function creepNameGenerator(prefix) {
  * Error codes:
  * -1: Creep not found
  * -2: Not enough energy to spawn creep
+ * -3: Invalid parameters
  */
-function createCreep(spawn, role) {
-    if (role in roles) {
-        var body = roles[role].build();
+function createCreep(spawn, creep) {
+    var memory = {};
+
+    if (typeof creep === "string") {
+        memory.role = creep;
+
+    } else if (typeof creep === "object") {
+        if (!creep.role) return -3;
+        if (creep.memory && typeof creep.memory === "object")
+            memory = _.cloneDeep(creep.memory);
+        memory.role = creep.role;
+
+    } else {
+        return -3;
+    }
+
+    if (memory.role in roles) {
+        var body = roles[memory.role].build();
 
         if (generics.getCreepCost(body) > spawn.energy) {
             return -2;
         }
 
-        var name = creepNameGenerator(role);
-        console.log('Spawner: Creating ' + role + ' ' + name);
+        var name = creepNameGenerator(memory.role);
+        console.log('Spawner: Creating ' + name);
 
-        var result = spawn.createCreep(body, name, {role:role});
+        var result = spawn.createCreep(body, name, memory);
 
         if (result !== name) {
             console.log('Spawner: Spawn error: ' + result);
@@ -64,12 +81,12 @@ function spawnAttempt(spawn, queue, priority) {
         result = createCreep(spawn, queue[i]);
 
         if (result == undefined) {
-            queue.splice(0, 1);
+            queue.splice(i, 1);
             return true;
         }
     }
 
-    if (i === queue.length || (i > 0 && priority)) {
+    if (i === queue.length || (queue.length > 0 && priority)) {
         return false;
     }
 }
