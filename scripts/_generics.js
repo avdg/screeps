1103937f998d79f025f855437e0fade12e2c6bdc
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2014-12-09 16:38:24 +0100

    Add generic functions logOnce and dontRepeat

--- scripts/_generics.js
+++ scripts/_generics.js
@@ -90,8 +90,67 @@ var parseCommand = function(command) {
 //console.log(parseCommand('test "\\\\mute\\"ed" sound'));
 //console.log(parseCommand('test "mute"sound'));
 
+/**
+ * Checks if a certain content has been found repeating itself
+ *
+ * A message is found not repeating if the message wasn't used in the current
+ * or previous round.
+ *
+ * @param string msg Message to check
+ * @param string namespace Namespace for categorization
+ *
+ * @return true if send, false if repeating, undefined if repeating in same round
+ */
+var dontRepeat = function(msg, namespace) {
+
+    // Tracker management
+    if (!(Memory.dontRepeat)) {
+        Memory.dontRepeat = {
+            time: Game.time,
+            logCurrent: {},
+            logPrevious: {},
+        };
+    } else if (Memory.dontRepeat.time !== Game.time) {
+        Memory.dontRepeat.logPrevious =
+            Memory.dontRepeat.time + 1 === Game.time ?
+                Memory.dontRepeat.logCurrent : {};
+        Memory.dontRepeat.logCurrent = {};
+        Memory.dontRepeat.time = Game.time;
+    }
+
+    // Get key
+    var key = namespace.replace('_', '') + '_' + msg;
+
+    // Run cache checks
+    if (key in Memory.dontRepeat.logCurrent) return;
+
+    Memory.dontRepeat.logCurrent[key] = true;
+    return !(key in Memory.dontRepeat.logPrevious);
+}
+
+/**
+ * Spamcontrolled console logging
+ *
+ * @param string msg
+ * @param bool warn Set to false to disable
+ *
+ * @return true if send, false if repeating, undefined if repeating in same round
+ */
+var logOnce = function(msg, warn) {
+    var result = dontRepeat(msg, 'log');
+
+    if (undefined === result && false !== warn)
+        console.log('Warning: reusing message "' + msg + '" in same round');
+    else if (result)
+        console.log(msg);
+
+    return result;
+};
+
 module.exports = {
     generator: generator,
     getCreepCost: getCreepCost,
     parseCommand: parseCommand,
+    dontRepeat: dontRepeat,
+    logOnce: logOnce,
 };
