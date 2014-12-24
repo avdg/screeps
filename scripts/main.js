b848c6cd3596983f1dd70249505c5347b5e26d83
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2014-12-24 02:28:31 +0100

    Put code between try/catch

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,32 @@
 var timerStart = performance.now();
 
-var settings = require('_settings');
+try {
+    var settings = require('_settings');
 
-var controller = require('stage_controller');
-var debug = require('stage_debug');
-var creeps = require('stage_creeps');
-var spawners = require('stage_spawners');
+    var controller = require('stage_controller');
+    var debug = require('stage_debug');
+    var creeps = require('stage_creeps');
+    var spawners = require('stage_spawners');
 
-debug();
-controller.pre();
-creeps();
-spawners();
-controller.post();
+    debug();
+    controller.pre();
+    creeps();
+    spawners();
+    controller.post();
+} catch (e) {
+    if (typeof e === "object") {
+        if (e.stack) {
+            if (e.name && e.message) {
+                console.log(e.name + ': ' + e.message);
+            }
+            console.log(e.stack);
+        } else {
+            console.log(JSON.stringify(e));
+        }
+    } else {
+        console.log(e);
+    }
+}
 
 var timerEnd = performance.now();
 var timerDiff = timerEnd - timerStart;
