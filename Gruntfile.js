02283c061fb133512f95e391fd708125d59f5ae3
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2015-05-03 19:01:27 +0200

    Add code coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,46 @@
 module.exports = function(grunt) {
 
-    grunt.loadNpmTasks('grunt-screeps');
+    grunt.loadNpmTasks('grunt-blanket');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-coveralls');
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-screeps');
 
     grunt.initConfig({
+        blanket: {
+            coverage: {
+                src: ['scripts/'],
+                dest: 'lib-cov/scripts/'
+            }
+        },
+        copy: {
+            coverage: {
+                expand: true,
+                src: ['test/**', 'lib/mocks/**'],
+                dest: 'lib-cov/'
+            }
+        },
+        coveralls: {
+            options: {
+                force: true
+            },
+            coverage: {
+                src: 'coverage/lcov.info'
+            }
+        },
         mochaTest: {
             test: {
                 src: 'test/**/*.js',
                 options: {
                     slow: 20
+                }
+            },
+            coverage: {
+                src: 'lib-cov/test/**/*.js',
+                options: {
+                    reporter: 'mocha-lcov-reporter',
+                    quiet: true,
+                    captureFile: 'coverage/lcov.info'
                 }
             }
         },
@@ -24,7 +56,10 @@ module.exports = function(grunt) {
     });
 
     grunt.task.registerTask('test', [
-        'mochaTest'
+        'blanket',
+        'copy',
+        'mochaTest',
+        'coveralls'
     ]);
 
     grunt.task.registerTask('default', [
