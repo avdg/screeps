4b403ad11f40128c4e119c2824055fefbf02e04f
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2015-05-12 17:48:45 +0200

    Require lodash in globals codegen

--- test/lib/codegen/globals.js
+++ test/lib/codegen/globals.js
@@ -7,19 +7,19 @@ var globalsCodegen = require('../../../lib/codegen/globals');
 describe('Codegen: globals', function() {
     describe('generate', function() {
         it('Should generate code', function() {
-            var result = "function() {\n    _.merge(AI, require('_generics'));\n    _.merge(AI, require('_utils'));\n    _.merge(AI, require('_settings'));\n}";
+            var result = "function() {\n    var _ = require('lodash');\n\n    _.merge(AI, require('_generics'));\n    _.merge(AI, require('_utils'));\n    _.merge(AI, require('_settings'));\n}";
 
             assert.equal(globalsCodegen.generate(), result);
         });
 
         it('Should generate adaptive code if provided option globalModules', function() {
-            var result = "function() {\n    _.merge(AI, require('foo'));\n}";
+            var result = "function() {\n    var _ = require('lodash');\n\n    _.merge(AI, require('foo'));\n}";
 
             assert.equal(globalsCodegen.generate({globalModules: ['foo']}), result);
         });
 
         it('Should generate code if options has globalModules and type is a string', function() {
-            var result = "function() {\n    _.merge(AI, require('foo'));\n}";
+            var result = "function() {\n    var _ = require('lodash');\n\n    _.merge(AI, require('foo'));\n}";
 
             assert.equal(globalsCodegen.generate({globalModules: 'foo'}), result);
         });
