770f7e6fb5fccc43c80e0827e9f9f66917c0452c
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2015-05-22 21:40:31 +0200

    Add target template

---
+++ extensions/documentation/targets/.FOO.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function filter(creep, opts) {
+    opts = opts || {};
+
+    return {
+        filter: function(target) {
+            // Insert code
+        }
+    };
+}
+
+// Room === undefined -> get all
+function get(room, option) {
+
+}
+
+module.exports = {
+    filter: filter,
+    get: get
+};
