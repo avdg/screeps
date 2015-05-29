44d8ad4ef51f79fba0327c082afb07a3da7af5d2
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2015-05-29 16:18:54 +0200

    Add more details about creeps in timer reports

--- scripts/stage_creeps.js
+++ scripts/stage_creeps.js
@@ -4,21 +4,40 @@ var roles = AI.extensions.roles;
 
 module.exports = function() {
     var i, length, role, creeps = AI.get('myCreeps');
+    var timer, timers = {};
 
     for (i = 0, length = creeps.length; i < length; i++) {
         var creep = creeps[i];
 
-        if (typeof creep.memory.role === "string") {
-            if (creep.spawning === true) {
-                if (typeof roles[creep.memory.role].spawning === "function") {
-                    roles[creep.memory.role].spawning(creep);
-                }
-            } else {
-                roles[creep.memory.role].turn(creep);
-            }
-        } else {
-            console.log('Warning: Ant without role');
+        if (typeof creep.memory.role !== "string") {
+            console.log('Warning: Creep without role');
+            continue;
         }
+
+        if (creep.spawning === true) {
+            if (typeof roles[creep.memory.role].spawning === "function") {
+                roles[creep.memory.role].spawning(creep);
+            }
+
+            continue;
+        }
+
+        // Bench function
+        timer = Game.getUsedCpu();
+        roles[creep.memory.role].turn(creep);
+        timer = AI.getTimeDiff(timer, Game.getUsedCpu());
+
+        // Keep records of all times
+        if (timers[creep.memory.role] === undefined) {
+            timers[creep.memory.role] = {
+                totalTime: timer,
+                timers: {}
+            };
+        } else {
+            timers[creep.memory.role].totalTime += timer;
+        }
+
+        timers[creep.memory.role].timers[creep.name] = timer;
     }
 
     for (role in roles) {
@@ -26,4 +45,6 @@ module.exports = function() {
             roles[role].endTurn();
         }
     }
+
+    return timers;
 };
