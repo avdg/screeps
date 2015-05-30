7e3c15e5def1bbf4ecde3c69e450f34d1d37a355
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2015-05-30 17:02:14 +0200

    Add countEmptyTilesAround

--- scripts/_utils.js
+++ scripts/_utils.js
@@ -2,6 +2,56 @@
 
 var _ = require('lodash');
 
+/** Counts empty tiles around a certain positions
+ *
+ * For now, everything that isn't counted as a wall
+ * as in terrain object is counted as free space.
+ *
+ * @param Number|Object x An object containing x, y and roomName or x position
+ * @param Number|undefined y Y position if x is not an object
+ * @param String|undefined room Room name if x is not an object
+ *
+ * @return Number The number of empty tiles
+ */
+function countEmptyTilesAround(x, y, room) {
+    var hasWall = function(list) {
+        for (var i = 0; i < list.length; i++) {
+            if (list[i].type === "terrain" && list[i].terrain === "wall") {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    if (typeof x === "object" &&
+        typeof x.x === "number" &&
+        typeof x.y === "number" &&
+        typeof x.roomName === "string"
+    ) {
+        room = x.roomName;
+        y = x.y;
+        x = x.x;
+    }
+
+    if (x < 1 || x > 48 || y < 1 || y > 49)
+        return;
+
+    var tiles = Game.rooms[room].lookAtArea(y - 1, x - 1, y + 1, x + 1);
+    var spaces = 0;
+
+    if (!hasWall(tiles[y - 1][x - 1])) spaces++;
+    if (!hasWall(tiles[y - 1][x]))     spaces++;
+    if (!hasWall(tiles[y - 1][x + 1])) spaces++;
+    if (!hasWall(tiles[y][x - 1]))     spaces++;
+    if (!hasWall(tiles[y][x + 1]))     spaces++;
+    if (!hasWall(tiles[y + 1][x - 1])) spaces++;
+    if (!hasWall(tiles[y + 1][x]))     spaces++;
+    if (!hasWall(tiles[y + 1][x + 1])) spaces++;
+
+    return spaces;
+}
+
 /**
  * Usage Calls command command_<parameter1> to execute its native property if available
  */
@@ -215,6 +265,7 @@ var manhattenDistance = function(x1, y1, x2, y2) {
 };
 
 module.exports = {
+    countEmptyTilesAround: countEmptyTilesAround,
     distance: distance,
     dontRepeat: dontRepeat,
     exec: exec,
