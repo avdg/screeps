40dfe4ec6f8ac55702629335c28c56b5c1bfc3dd
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2015-06-28 12:28:14 +0200

    Add arguments support for hooks

--- extensions/tools/library/hooks.js
+++ extensions/tools/library/hooks.js
@@ -1,9 +1,17 @@
 'use strict';
 
 function emit(f) {
+    var args = Array(arguments.length - 1);
+    console.log(args);
+
+    var i = 0;
+    while (++i < arguments.length) {
+        args[i - 1] = arguments[i];
+    }
+
     for (var hook in AI.extensions.hooks) {
         if (typeof AI.extensions.hooks[hook][f] === "function") {
-            AI.extensions.hooks[hook][f]();
+            AI.extensions.hooks[hook][f].apply(undefined, args);
         }
     }
 }
--- test/extensions/tools/library/hooks.js
+++ test/extensions/tools/library/hooks.js
@@ -6,23 +6,51 @@ var simple = require('simple-mock');
 var lib = require('../../../../extensions/tools/library/hooks');
 
 describe('Library extensions: hooks', function() {
-    AI.extensions.hooks = {
-        me: {
-            test: function() {},
-            fail: function() {}
-        },
-        you: {
-            hi: function() {},
-        }
-    };
+    it('Should call the right hooks', function() {
+        global.AI = {
+            extensions: {
+                hooks: {
+                    me: {
+                        test: function() {},
+                        fail: function() {}
+                    },
+                    you: {
+                        hi: function() {},
+                    }
+                }
+            }
+        };
 
-    var fn1 = simple.mock(AI.extensions.hooks.me, 'test');
-    var fn2 = simple.mock(AI.extensions.hooks.me, 'fail');
-    var fn3 = simple.mock(AI.extensions.hooks.you, 'hi');
+        var fn1 = simple.mock(AI.extensions.hooks.me, 'test');
+        var fn2 = simple.mock(AI.extensions.hooks.me, 'fail');
+        var fn3 = simple.mock(AI.extensions.hooks.you, 'hi');
 
-    assert.strictEqual(lib.emit('test'), undefined);
+        assert.strictEqual(lib.emit('test'), undefined);
 
-    assert.equal(fn1.callCount, 1);
-    assert.equal(fn2.callCount, 0);
-    assert.equal(fn3.callCount, 0);
+        assert.equal(fn1.callCount, 1);
+        assert.equal(fn2.callCount, 0);
+        assert.equal(fn3.callCount, 0);
+
+        assert.deepEqual(fn1.calls[0].args, []);
+    });
+
+    it('Should be able to pass parameters through emit', function() {
+        global.AI = {
+            extensions: {
+                hooks: {
+                    test: {
+                        callHook: function() {}
+                    }
+                }
+            }
+        };
+
+        var fn1 = simple.mock(AI.extensions.hooks.test, 'callHook');
+
+        debugger;
+        assert.strictEqual(lib.emit('callHook', 1, 2, 3), undefined);
+
+        assert.equal(fn1.callCount, 1);
+        assert.deepEqual(fn1.calls[0].args, [1, 2, 3]);
+    })
 });
