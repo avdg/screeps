094345a8f97390e823fa4e55876e64a7d24a87bb
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2015-07-16 12:29:39 +0200

    Add role dummy

---
+++ extensions/tools/roles/dummy.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * Returns body parts required to build the ant
+ *
+ * Spawn: null or spawn
+ */
+function build(spawn) {
+    return [
+        TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE
+    ];
+}
+
+/**
+ * Executed after ant being spawned
+ */
+function init(name) {
+
+}
+
+/**
+ * Executed each turn
+ */
+function turn(creep) {
+
+}
+
+/**
+ * Triggered after all creeps are processed
+ *
+ * Usefull with a file scoped variable to store creeps and then do something
+ * with all of them at once, like picking the closest creep to a certain position
+ */
+ function endTurn() {
+
+ }
+
+module.exports = {
+    role: 'dummy',
+    build: build,
+    init: init,
+    turn: turn,
+    endTurn: endTurn
+};
