f26a3e102a432224ccd836662cdaeab5a4942ae6
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2015-08-21 02:12:22 +0200

    Add optional spawn parameter for addQueue and addPriorityQueue commands

--- extensions/tools/commands/addPriorityQueue.js
+++ extensions/tools/commands/addPriorityQueue.js
@@ -7,5 +7,5 @@ function command(flag, parameters) {
 module.exports = {
     exec: command,
     command: "addPriorityQueue",
-    help: 'Description:\n- Adds an ant to a spawnPriorityQueue\n\nUsage:\n- addPriorityQueue &lt;role&gt;',
+    help: 'Description:\n- Adds an ant to a spawnPriorityQueue\n\nUsage:\n- addPriorityQueue &lt;role&gt; [spawn]',
 };
